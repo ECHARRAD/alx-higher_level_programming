772ab9de3d63c6593fdadf5c58d35291166b4c70
ECHARRAD <echarradahmed@gmail.com>
2023-08-11 08:35:26 -0700

    gone

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from add_0 import add
-    a = 1
-    b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
+
+import math
+
+def add(a, b):
+    return a + b
+
+a = 1
+b = 2
+
+res = add(a, b)
+
+print(f"{a} + {b} = {res}")
